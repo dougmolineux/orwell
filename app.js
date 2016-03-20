84162f78cf8ba4fbb77fa26247d9e0bc1698444d
Doug Molineux <dougmolineux@Dougs-MacBook-Pro.local>
2016-03-19 19:11:44 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Example Usage:
+// node app.js admin myPassword 10.0.0.159
+
+var request = require("request");
+var MjpegConsumer = require("mjpeg-consumer");
+var MotionStream = require("motion-detect").Stream;
+var FileOnWrite = require("file-on-write");
+var consumer = new MjpegConsumer();
+var motion = new MotionStream();
+
+var writer = new FileOnWrite({ 
+  path: './video',
+  ext: '.jpg',
+  filename: function(image) {
+    var fn = image.time;
+    console.log("Writing to file: " + fn);
+    return fn;
+  },
+  transform: function(image) {
+    return image.data;
+  },
+  sync: true
+});
+
+var username = process.argv[2];
+var password = process.argv[3];
+var url = "http://"+process.argv[4]+"/mjpeg.cgi";
+
+console.log("Attempting to connect to "+url+"...")
+
+var options = {
+  url: url,
+  headers: {
+    'Authorization': 'Basic ' + new Buffer(username + ':' + password).toString('base64')
+  }  
+};
+
+request(options).pipe(consumer).pipe(motion).pipe(writer);
---
+++ createGif.js
@@ -0,0 +1,10 @@
+var exec = require('child_process').exec;
+var filename = new Date().getTime() + ".gif";
+var cmd = 'convert -delay 2 -loop 0 "video/*.jpg" ' + filename;
+
+exec(cmd, function(error, stdout, stderr) {
+	console.log("error", error);
+	console.log("stdout", stdout);
+	console.log("stderr", stderr);
+	console.log("Finished making " + filename);
+});

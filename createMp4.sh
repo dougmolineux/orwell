e246bbf42c7f3f1e91500817ba88c0ff28976e84
Doug Molineux <dougmolineux@Dougs-MacBook-Pro.local>
2016-04-20 12:16:32 -0600

    Implemented timestamping into createMp4.sh

--- createMp4.sh
+++ createMp4.sh
@@ -2,5 +2,5 @@ which ffmpeg
 cd video
 ls -1 | wc -l
 cd ..
-mv video video2
-ffmpeg -pattern_type glob -i 'video2/*.jpg' movie.mp4
+node timestampImage.js
+ffmpeg -pattern_type glob -i 'timestamped_video/*.jpg' movie.mp4

867cd0d5a56807ca8580967398faf6f08f5da12a
dougmolineux <doug.molineux@gmail.com>
2016-04-21 19:22:03 -0600

    Added some additional cleanup

--- makeMp4.js
+++ makeMp4.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const spawn = require('child_process').spawn;
 const filename = new Date().getTime().toString() + ".mp4"
+const exec = require('child_process').exec;
 
 function run() {
 	const cmd = spawn('bash', [ __dirname + '/ffmpegWrapper.sh', filename ]);
@@ -16,6 +17,9 @@ function run() {
 		fs.stat(filename, (err, stat) => {
 			if(err == null) {
 				console.log('Success! ' + filename + ' Confirmed to exist');
+				exec("rm -rf timestamped_video/*.jpg", (err, stdout, stderr) => {
+                                        console.log("Removed jpgs in timestamped_video")
+                                });	
 			} else {
 				console.log(filename + ' Does Not seem to exist!');
 			}
